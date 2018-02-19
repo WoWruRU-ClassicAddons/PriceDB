927d76e130fa28fb84f6dc3ab0ded8cc888fd32b
Артур <Artur91425@gmail.com>
2018-02-19 22:21:14 +0300

    Update
    Added print ID on ShiftClick
    Fixed TooltipWidth

--- PriceDB/PriceDB.lua
+++ PriceDB/PriceDB.lua
@@ -6,9 +6,15 @@ PriceDB.sellvalue = CreateFrame("Frame" , "PriceDBGameTooltip", GameTooltip)
 if GetLocale() == "ruRU" then
 	PriceDB_Sell = "Продажа"
 	PriceDB_Buy = "Покупка"
+	PriceDB_g = "з"
+	PriceDB_s = "с"
+	PriceDB_c = "м"
 else
 	PriceDB_Sell = "Sell"
 	PriceDB_Buy = "Buy"
+	PriceDB_g = "g"
+	PriceDB_s = "s"
+	PriceDB_c = "c"
 end
 
 PriceDB.sellvalue:SetScript("OnHide", function()
@@ -17,47 +23,47 @@ PriceDB.sellvalue:SetScript("OnHide", function()
 end)
 
 PriceDB.sellvalue:SetScript("OnShow", function()
-    if GameTooltip.itemLink then
-		local _, _, itemID = string.find(GameTooltip.itemLink, "item:(%d+):%d+:%d+:%d+")
-		local itemID = tonumber(itemID)
-		local count = GameTooltip.itemCount or 1
-		
-		if SellData[itemID] then
-			local _, _, sell, buy = strfind(SellData[itemID], "(.*),(.*)")
-			sell = tonumber(sell)
-			buy = tonumber(buy)
-			
-			if not MerchantFrame:IsShown() then
-				if sell > 0 then PriceDB:SetTooltipMoney(GameTooltip, sell*count) end
-			end
-			if IsShiftKeyDown() then
-				GameTooltip:AddLine(" ")
-				
-				if count > 1 then
-					GameTooltip:AddDoubleLine(PriceDB_Sell..":", PriceDB:CreateGoldString(sell).."|cff555555  //  "..PriceDB:CreateGoldString(sell*count), 1, 1, 1)
-				else
-					GameTooltip:AddDoubleLine(PriceDB_Sell..":", PriceDB:CreateGoldString(sell*count), 1, 1, 1)
-				end
-				
-				if count > 1 then
-					GameTooltip:AddDoubleLine(PriceDB_Buy..":", PriceDB:CreateGoldString(buy).."|cff555555  //  "..PriceDB:CreateGoldString(buy*count), 1, 1, 1)
-				else
-					GameTooltip:AddDoubleLine(PriceDB_Buy..":", PriceDB:CreateGoldString(buy), 1, 1, 1)
-				end
-			end
-			GameTooltip:Show()
+    if not GameTooltip.itemLink then return end
+	local _, _, itemID = string.find(GameTooltip.itemLink, "item:(%d+):%d+:%d+:%d+")
+	local itemID = tonumber(itemID)
+	local count = GameTooltip.itemCount or 1
+	PriceDB:SetItemID(itemID, GameTooltipTextLeft1:GetText())
+	
+	if not SellData[itemID] then return end
+	local _, _, sell, buy = string.find(SellData[itemID], "(.*),(.*)")
+		sell = tonumber(sell)
+		buy = tonumber(buy)
+
+	if not MerchantFrame:IsShown() then
+		if sell > 0 then PriceDB:SetTooltipMoney(GameTooltip, sell*count) end
+	end
+	if IsShiftKeyDown() then
+		GameTooltip:AddLine(" ")
+		if count > 1 then
+			GameTooltip:AddDoubleLine(PriceDB_Sell..":", PriceDB:CreateGoldString(sell).."|cff555555  //  "..PriceDB:CreateGoldString(sell*count), 1, 1, 1)
+			GameTooltip:AddDoubleLine(PriceDB_Buy..":", PriceDB:CreateGoldString(buy).."|cff555555  //  "..PriceDB:CreateGoldString(buy*count), 1, 1, 1)
+		else
+			GameTooltip:AddDoubleLine(PriceDB_Sell..":", PriceDB:CreateGoldString(sell*count), 1, 1, 1)
+			GameTooltip:AddDoubleLine(PriceDB_Buy..":", PriceDB:CreateGoldString(buy), 1, 1, 1)
 		end
 	end
+	GameTooltip:Show()
 end)
 
+function PriceDB:SetItemID(itemID, itemName)
+	if IsShiftKeyDown() then
+		local newFirstLine = GameTooltipTextLeft1:SetText("|cffFFD100["..itemID.."]|r "..itemName)
+	end
+end
+
 function PriceDB:SetTooltipMoney(frame, money)
-	frame:AddLine(SALE_PRICE_COLON.." ", 1.0, 1.0, 1.0)
-	local numLines = frame:NumLines()
+	frame:AddLine(SALE_PRICE_COLON.." ", 1, 1, 1)
+	local lastLine = getglobal(frame:GetName().."TextLeft"..frame:NumLines())
 	local moneyFrame = getglobal(frame:GetName().."MoneyFrame")
-	moneyFrame:SetPoint("LEFT", frame:GetName().."TextLeft"..numLines, "RIGHT", 4, 0)
+	moneyFrame:SetPoint("LEFT", lastLine, "RIGHT", 4, 0)
 	moneyFrame:Show()
 	MoneyFrame_Update(moneyFrame:GetName(), money)
-	frame:SetMinimumWidth(moneyFrame:GetWidth())
+	frame:SetMinimumWidth(lastLine:GetWidth() - 10 + moneyFrame:GetWidth())
 end
 
 function PriceDB:CreateGoldString(money)
@@ -66,9 +72,9 @@ function PriceDB:CreateGoldString(money)
 	local copper = floor(mod(money,100))
 	
 	local string = ""
-	if gold > 0 then string = string .. "|cffffffff" .. gold .. "|cffffd700g" end
-	if silver > 0 then string = string .. "|cffffffff " .. silver .. "|cffc7c7cfs" end
-	string = string .. "|cffffffff " .. copper .. "|cffeda55fc"
+	if gold > 0 then string = string .. "|cffffffff" .. gold .. "|cffffd700"..PriceDB_g end
+	if silver > 0 then string = string .. "|cffffffff " .. silver .. "|cffc7c7cf"..PriceDB_s end
+	string = string .. "|cffffffff " .. copper .. "|cffeda55f"..PriceDB_c
 	
 	return string
 end
@@ -76,7 +82,7 @@ end
 local function GetItemLinkByName(name)
 	for itemID = 1, 25818 do
 		local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
-		if (itemName and itemName == name) then
+		if itemName and itemName == name then
 			local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
 			return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
 		end
